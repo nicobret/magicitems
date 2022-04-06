318d1739c1a382fa0ff8839144bdac155f9050dd
Nicolas Bretécher <bretechern@gmail.com>
2022-04-06 10:49:22 +0200

    initial commit

---
+++ database/migrations/2022_03_11_210948_items.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->string('description');
+            $table->boolean('requires_attunement')->nullable()->default(false);
+            $table->integer('rarity_id')->unsigned()->nullable()->default(12);
+            $table->integer('type_id')->unsigned()->nullable()->default(12);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+};
